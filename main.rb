b23181393ee353e6979c9bd1647ae31ce62905cd
Obi Akubue <obioraakubue@yahoo.com>
2010-11-14 21:15:11 -0500

    Changes to get closer to a release

--- main.rb
+++ main.rb
@@ -3,14 +3,13 @@ require "bundler/setup"
 
 require 'lib/ragios'
 
-
-class TestMySite < Ragios::Monitors::TestURL    
+class TestMySite  <  Ragios::Monitors::TestURL   
    def initialize
       @time_interval = '20m'
       @notification_interval = '6h'
       @contact = "obi@mail.com"	
       @test_description  = "My Website Test"
-      @test_url = "http://www.whisperservers.com"  
+      @test_url = "http://www.whisperservers.com" 
       super
    end
 end
@@ -29,17 +28,16 @@ end
   
 class TestMyBlog < Ragios::Monitors::TestHTTP
    def initialize
-      @time_interval = '15s'
-      @notification_interval = '30s'
+      @time_interval = '20m'
+      @notification_interval = '6h'
       @contact = "obi@mail.com"
       @test_description = "Http connection to my blog"
-      @test_domain = "obi-akubue.homeunix.org"
+      @test_domain = "obi-akubue.org"
       super
    end
 end
 
-
-tests = [ TestMySite.new, TestMyBlog.new, TestFakeSite.new]
+tests = [TestMySite.new, TestMyBlog.new, TestFakeSite.new]
 
 ragios = Ragios::Schedulers::RagiosScheduler.new tests 
 ragios.init
