2b123e31066792043693fe99f16840743e74adc2
Obi Akubue <obioraakubue@yahoo.com>
2011-01-10 16:18:01 -0500

    Clean up

--- dsl.rb
+++ dsl.rb
@@ -7,40 +7,6 @@
   require dir + 'lib/ragios'
     
 
-     monitoring =  { :monitor => 'url',
-                   :every => '30s',
-                   :test => 'my personal blog',
-                   :url => 'http://obi-akubue.org',
-                   :contact => 'obi.akubue@mail.com',
-                   :via => 'gmail',  
-                   :notify_interval => '6h'
-                  
-                  },
-                  { :monitor => 'url',
-                   :every => '2m',
-                   :test => 'My Website Test',
-                   :url => 'http://www.whisperservers.com/blog/',
-                   :contact => 'obi.akubue@mail.com',
-                   :via => 'gmail',  
-                   :notify_interval => '6h'
-                  
-                  },
-                  {  :monitor => 'process',
-                   :every => '1m',
-                   :test => 'Apache Test',
-                   :process_name => 'apache2',
-                   :start_command => 'sudo /etc/init.d/apache2 start',
-                   :stop_command => 'sudo /etc/init.d/apache2 stop',
-                   :restart_command => 'sudo /etc/init.d/apache2 restart',
-                   :pid_file => '/var/run/apache2.pid',
-                   :server_alias => 'my home server',
-                   :hostname => '192.168.2.9',
-                   :contact => 'obi.akubue@mail.com',
-                   :via => 'gmail',  
-                   :notify_interval => '2m'
-                  }
- 
-  Ragios::Monitor.start monitoring
 
 
 
