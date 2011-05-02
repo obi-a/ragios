e5b5b4e8b556f610543080b0dd1b945ff9706226
Obi Akubue <obioraakubue@yahoo.com>
2011-05-02 04:17:31 -0400

    sample test

---
+++ myconfig.rb
@@ -0,0 +1,42 @@
+  #ragios/main.rb
+  require 'rubygems'
+  require "bundler/setup"
+  
+  dir = Pathname(__FILE__).dirname.expand_path
+  require dir + 'lib/ragios'
+
+  #Add your code here
+
+
+ monitoring   = [{:monitor =>'http',
+                   :every => '5m',
+                   :test => 'Http connection to my site',
+                   :domain => 'www.google.com',
+                   :contact => 'admin@mysite.com',
+                   :via => 'gmail',
+                   :notify_interval => '6h'
+                  }]
+
+  Ragios::Monitor.start monitoring
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ #trap Ctrl-C to exit gracefully
+    puts "PRESS CTRL-C to QUIT"
+     loop do
+       trap("INT") { puts "\nExiting"; exit; }
+     sleep(3)
+    end
