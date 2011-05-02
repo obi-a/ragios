96eb012578707a99ccd30546f107770ccc2ae3c3
Obi Akubue <obioraakubue@yahoo.com>
2011-05-02 04:09:15 -0400

    added a new folder for plugins

---
+++ lib/ragios/monitors/plugins/http.rb
@@ -0,0 +1,33 @@
+module Monitors
+
+# Plugin: Monitors a domain to check if its online  
+# It establishes a HTTP connection to the domain
+# PASSED if it establishes the HTTP connection successfully and FAILED if it throws an exception
+class Http 
+   attr_reader :domain 
+   attr_accessor :describe_test_result 
+  
+   def init(options)
+        @domain = options[:domain] 
+        raise "@domain must be assigned a value" if @domain.nil?
+        @describe_test_result = "HTTP Connection to " + @domain     
+   end 
+
+   #connects to the domain via HTTP
+   #PASSED when it establishes a successful HTTP connection with the domain
+   def test_command
+     begin 
+           
+          Net::HTTP.start(@domain) 
+          @test_result = 'PASSED'
+          return TRUE 
+
+     rescue Exception
+            @test_result =  $! # $! global variable reference to the Exception object
+            return FALSE  
+     end      
+   end   
+end
+
+end
+
