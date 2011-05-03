9fbfde3b5a3a7052f43d1096e414bb1299b250cb
Obi Akubue <obioraakubue@yahoo.com>
2011-05-03 05:04:18 -0400

    Removed hash rockets from the DSL syntax.

--- myconfig.rb
+++ myconfig.rb
@@ -7,35 +7,21 @@
 
   #Add your code here
 
- monitoring   = {:monitor =>'http',
-                   :every => '5m',
-                   :test => 'Http connection to my site',
-                   :domain => 'www.google.com',
-                   :contact => 'admin@mysite.com',
-                   :via => 'gmail',
-                   :notify_interval => '6h'
+ monitoring   = { monitor: 'http',
+                  every: '5m',
+                  test: 'Http connection to my site',
+                  domain: 'www.google.com',
+                  contact: 'admin@mysite.com',
+                   via: 'gmail',
+                   notify_interval: '6h'
                   },
-                   { :monitor => 'url',
-                   :every => '5m',
-                   :test => 'video datafeed test',
-                   :url => 'http://www.google.com',
-                   :contact => 'admin@mail.com',
-                   :via => 'gmail',
-                   :notify_interval => '6h'
-                  },
-                  {  :monitor => 'process',
-                   :every => '5m',
-                   :test => 'Apache Test',
-                   :process_name => 'apache2',
-                   :start_command => '/etc/init.d/apache2 start',
-                   :stop_command => '/etc/init.d/apache2 stop',
-                   :restart_command => '/etc/init.d/apache2 restart',
-                   :pid_file => '/var/run/apache2.pid',
-                   :server_alias => 'my home server',
-                   :hostname => '192.168.2.9',
-                   :contact => 'obi.akubue@mail.com',
-                   :via => 'gmail',
-                   :notify_interval => '2m'
+                 { monitor: 'url',
+                   every: '5m',
+                   test: 'video datafeed test',
+                   url: 'http://www.google.com',
+                   contact: 'admin@mail.com',
+                   via: 'gmail',
+                   notify_interval: => '6h'
                   }
                    
   Ragios::Monitor.start monitoring
