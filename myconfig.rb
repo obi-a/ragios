11f177e52896949620994d0c124f49280df32c2d
Obi Akubue <obioraakubue@yahoo.com>
2011-05-02 21:36:45 -0400

    added a url plugin sample

--- myconfig.rb
+++ myconfig.rb
@@ -7,15 +7,22 @@
 
   #Add your code here
 
-
- monitoring   = [{:monitor =>'http',
+ monitoring   = {:monitor =>'http',
                    :every => '5m',
                    :test => 'Http connection to my site',
                    :domain => 'www.google.com',
                    :contact => 'admin@mysite.com',
                    :via => 'gmail',
                    :notify_interval => '6h'
-                  }]
+                  },
+                   { :monitor => 'url',
+                   :every => '5m',
+                   :test => 'video datafeed test',
+                   :url => 'http://www.google.com',
+                   :contact => 'admin@mail.com',
+                   :via => 'gmail',
+                   :notify_interval => '6h'
+                  }
 
   Ragios::Monitor.start monitoring
 
