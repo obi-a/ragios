dccd4ef4375477d45bc75818f395a17efefe3634
Obi Akubue <obioraakubue@yahoo.com>
2011-05-08 00:14:11 -0400

    Added a config file for Ragios config settings

---
+++ test/config.rb
@@ -0,0 +1,17 @@
+#Replace with Gmail username and password
+gmail_account = {username: 'Gmail Username',
+                 password: 'Gmail Password'} 
+
+
+#Ragios::GmailNotifier.config(gmail_account)
+
+
+#replace with twitter credentials
+twitter_cred = {consumer_key: 'Consumer Key', 
+               consumer_secret: 'Consumer secret',
+                 access_token: 'Access Token',
+                 access_secret: 'Access Secret'}
+
+#Ragios::TwitterNotifier.config(twitter_cred)  
+
+
