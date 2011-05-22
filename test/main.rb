8563f7c255924d147483f87a9f05b80c2f610b6c
Obi Akubue <obioraakubue@yahoo.com>
2011-05-22 03:41:07 -0400

    testing

--- test/main.rb
+++ test/main.rb
@@ -16,8 +16,8 @@
                        }
 
   monitoring = { monitor: 'url',
-                   every: '1h',
-                   test: 'XYZ data feed',
+                   every: '1m',
+                   test: '1 test feed',
                    url: 'http://www.website.com/89843/videos.xml',
                    contact: 'obi.akubue@mail.com',
                    via: 'gmail',  
@@ -27,7 +27,7 @@
                     },
                   { monitor: 'url',
                    every: '1m',
-                   test: 'github repo a http test',
+                   test: '2 test',
                    url: 'https://github.com/obi-a/Ragios',
                    contact: 'obi.akubue@mail.com',
                    via: 'gmail',  
@@ -38,6 +38,30 @@
 
   Ragios::Monitor.start monitoring
 
+
+   monitoring = { monitor: 'url',
+                   every: '1m',
+                   test: '3 test',
+                   url: 'http://www.website.com/89843/videos.xml',
+                   contact: 'obi.akubue@mail.com',
+                   via: 'gmail',  
+                   notify_interval: '6h',
+                   failed:run_when_failed, 
+                   fixed:run_on_fixed  
+                    },
+                  { monitor: 'url',
+                   every: '1m',
+                   test: '4 test',
+                   url: 'https://github.com/obi-a/Ragios',
+                   contact: 'obi.akubue@mail.com',
+                   via: 'gmail',  
+                   notify_interval:'3m',
+                   failed:run_when_failed, 
+                   fixed:run_on_fixed 
+                  }
+
+   
+   Ragios::Monitor.start monitoring
 
  #trap Ctrl-C to exit gracefully
     puts "PRESS CTRL-C to QUIT"
