0e4f8b925e9fd4aef014adfec7358f790aff491f
Obi Akubue <obioraakubue@yahoo.com>
2011-05-26 20:57:26 -0400

    Use Yajl gem to symbolize hash keys from Json data

--- ragios_server.rb
+++ ragios_server.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require "bundler/setup"
-  dir = Pathname(__FILE__).dirname.expand_path
-  require dir + 'lib/ragios'
-  require dir + 'config'
+dir = Pathname(__FILE__).dirname.expand_path
+require dir + 'lib/ragios'
+require dir + 'config'
 require 'sinatra' 
 require 'yajl'
 
@@ -14,23 +14,8 @@ end
 
 put '/ragios/monitor' do
 
-  json_style_hash = Yajl::Parser.parse(request.body.read)
-#will clean up the code later, still buggy
-  new_hash = {}
-  monitors = []
-  count = 0
+  monitors = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
+  Ragios::Monitor.start monitors
 
-  json_style_hash.each do|hash|
-      hash.each do |key,value|
-        key = key.to_sym
-        new_hash[key] = value
-      end
-
-   monitors[count] =  new_hash
-   count += 1
-  end
-  Ragios::Monitor.start monitors 
-
-   puts monitors.inspect 
   "{\"ok\":true}"
 end
