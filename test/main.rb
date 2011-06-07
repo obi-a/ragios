c4f15e9296c34eb5f393d6bfc95d61701b4082f3
Obi Akubue <obioraakubue@yahoo.com>
2011-06-07 03:00:48 -0400

    sample data

--- test/main.rb
+++ test/main.rb
@@ -4,6 +4,8 @@
   require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib/ragios'))
   require  Pathname(__FILE__).dirname.expand_path + 'config' 
 
+  require 'yajl'
+
  #Add your code here
  run_when_failed = lambda{ 
                             puts 'switch to backup datafeed'
@@ -21,9 +23,7 @@
                    url: 'http://www.website.com/89843/videos.xml',
                    contact: 'obi.akubue@mail.com',
                    via: 'gmail',  
-                   notify_interval: '6h',
-                   failed:run_when_failed, 
-                   fixed:run_on_fixed  
+                   notify_interval: '6h'
                     },
                   { monitor: 'url',
                    every: '1m',
@@ -31,42 +31,37 @@
                    url: 'https://github.com/obi-a/Ragios',
                    contact: 'obi.akubue@mail.com',
                    via: 'gmail',  
-                   notify_interval:'3m',
-                   failed:run_when_failed, 
-                   fixed:run_on_fixed 
+                   notify_interval:'3m'
                   }
 
-  Ragios::Monitor.start monitoring
+  #Ragios::Monitor.start monitoring
 
 
-   monitoring = { monitor: 'url',
+   monitoring = [{ monitor: 'url',
                    every: '1m',
                    test: '3 test feed',
                    url: 'http://www.website.com/89843/videos.xml',
                    contact: 'obi.akubue@mail.com',
                    via: 'gmail',  
-                   notify_interval: '6h',
-                   failed:run_when_failed, 
-                   fixed:run_on_fixed  
-                    },
-                  { monitor: 'url',
-                   every: '1m',
-                   test: '4 test',
-                   url: 'https://github.com/obi-a/Ragios',
-                   contact: 'obi.akubue@mail.com',
-                   via: 'gmail',  
-                   notify_interval:'3m',
-                   failed:run_when_failed, 
-                   fixed:run_on_fixed 
-                  }
+                   notify_interval: '6h'
+                  
+                    }]
 
-  monitors = Ragios::Monitor.start monitoring
-       
-      monitors.each do |monitor|  
-         puts monitor.test_description 
-         puts monitor.creation_date
-         puts monitor.time_of_last_test
-      end
+ Ragios::Monitor.start monitoring
+
+   monitors = Ragios::Monitor.get_monitors
+    puts Yajl::Encoder.encode(monitors)
+   #puts monitors.inspect
+      # hash = {}
+    #  monitors.each do |monitor|  
+      #monitor.instance_variables.each {|var| hash[var.to_s.delete("@")] = monitor.instance_variable_get(var) }
+     # monitor.instance_variables.each {|var| hash[var[1..-1].to_sym] = monitor.instance_variable_get(var) }
+     # puts hash.inspect
+
+     #    puts monitor.test_description 
+     #    puts monitor.creation_date
+      #   puts monitor.time_of_last_test
+    # end
 
  #trap Ctrl-C to exit gracefully
     puts "PRESS CTRL-C to QUIT"
