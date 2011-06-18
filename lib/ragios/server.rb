e394a5988dd3f96ac7f150791922099d872b18a2
Obi Akubue <obioraakubue@yahoo.com>
2011-06-18 10:43:09 -0400

    programming interface to Ragios Server

---
+++ lib/ragios/server.rb
@@ -0,0 +1,32 @@
+module Ragios 
+
+#hides the messy details of the scheduler from users 
+#provides an easy interface to start monitoring the system by calling Ragios::Server start monitoring 
+ class Server
+   
+    attr_accessor :ragios
+    
+    def initialize
+
+    end
+
+   def self.update_status config
+       #@ragios.update_status config
+   end
+    
+    #returns a list of active monitors
+    def self.get_monitors
+       # @ragios.get_monitors
+    end
+
+    def self.start monitoring
+        
+     @ragios = Ragios::Schedulers::Server.new monitoring
+     #@ragios.init
+     #@ragios.start 
+     #returns a list of active monitors
+     #@ragios.get_monitors
+    end
+ end
+
+end
