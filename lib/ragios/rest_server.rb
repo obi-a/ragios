6fe6c7819e90d1c758ee6f7bf64f80f1e99e9bc5
Obi Akubue <obioraakubue@yahoo.com>
2011-09-24 13:14:49 -0400

    added cleaner and simpler more RESTful API calls

--- lib/ragios/rest_server.rb
+++ lib/ragios/rest_server.rb
@@ -14,13 +14,13 @@ require 'yajl'
     
 # end
 
-
+#TODO set Content-Type and other header attributes for each request
 get '/' do
   Yajl::Encoder.encode({ "Ragios Server" => "welcome"})
 end
 
 #adds monitors to the system and starts monitoring them
-post '/monitors' do
+post '/monitors*' do
  begin
   monitors = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
   Ragios::Monitor.start monitors,server=TRUE
@@ -43,19 +43,7 @@ get '/monitors/:key/:value*' do
     end
 end
 
-put  '/monitor/:id*' do
-  begin
-    data = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
-    id = params[:id]
-    hash = Ragios::Server.update_monitor(id,data)
-    Yajl::Encoder.encode({ "ok" => "true"})
-  rescue 
-  status 500
-  body  Yajl::Encoder.encode({error: "something went wrong"})
- end
-end
-
-delete '/monitor/:id*' do
+delete '/monitors/:id*' do
    id = params[:id]
    hash = Ragios::Server.delete_monitor(id)
    if hash.to_s == "not_found"  
@@ -69,7 +57,8 @@ delete '/monitor/:id*' do
   end
 end
 
-post '/monitor/:id/stop*' do
+#stop a running monitor
+put '/monitors/:id/state/stopped*' do
    id = params[:id]
    hash = Ragios::Server.stop_monitor(id)
    if hash.to_s == "not_found"  
@@ -83,7 +72,8 @@ post '/monitor/:id/stop*' do
   end
 end
 
-post '/monitor/:id/restart*' do
+#restart a running monitor
+put '/monitors/:id/state/active*' do
   begin 
    id = params[:id]
    Ragios::Server.restart_monitor(id)
@@ -94,7 +84,19 @@ post '/monitor/:id/restart*' do
     end
 end
 
-get '/monitor/:id*' do
+put  '/monitors/:id*' do
+  begin
+    data = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
+    id = params[:id]
+    hash = Ragios::Server.update_monitor(id,data)
+    Yajl::Encoder.encode({ "ok" => "true"})
+  rescue 
+  status 500
+  body  Yajl::Encoder.encode({error: "something went wrong"})
+ end
+end
+
+get '/monitors/:id*' do
   begin
    id = params[:id]
    monitor = Ragios::Server.get_monitor(id)
@@ -121,7 +123,7 @@ get '/monitors*' do
 end
 
 #status updates
-get '/status_update/:key/:value*' do
+get '/status_updates/:key/:value*' do
  key = params[:key].to_sym
  value = params[:value]
  monitors = Ragios::Server.find_status_update(key => value)
@@ -134,7 +136,7 @@ get '/status_update/:key/:value*' do
  end
 end
 
-post '/status_update/start*' do
+post '/status_updates*' do
   begin
    config = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
    hash = Ragios::Server.start_status_update(config)
@@ -146,7 +148,8 @@ post '/status_update/start*' do
 
 end
 
-post '/status_update/:tag/restart*' do
+#restart a status update
+put '/status_updates/:tag/state/active*' do
    tag = params[:tag]
    update = Ragios::Server.restart_status_updates(tag)
    if update == nil 
@@ -157,7 +160,8 @@ post '/status_update/:tag/restart*' do
   end
 end
 
-post '/status_update/:tag/stop*' do
+#stop a status update
+put '/status_updates/:tag/state/stopped*' do
    tag = params[:tag]
    update = Ragios::Server.stop_status_update(tag)
    if update == []
@@ -169,7 +173,7 @@ post '/status_update/:tag/stop*' do
 end
 
 #delete status update by tag
-delete '/status_update/:tag*' do
+delete '/status_updates/:tag*' do
    tag = params[:tag]
    update = Ragios::Server.delete_status_update(tag)
    if update == []
@@ -181,7 +185,7 @@ delete '/status_update/:tag*' do
 end
 
 #edit status update
-put '/status_update/:id*' do
+put '/status_updates/:id*' do
  begin
    data = Yajl::Parser.parse(request.body.read, :symbolize_keys => true)
    id = params[:id]
