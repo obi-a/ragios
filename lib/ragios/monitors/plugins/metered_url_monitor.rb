199f55b6af9bbaa0aa49996ebf588d61624acfbc
Obi Akubue <obioraakubue@yahoo.com>
2012-02-02 14:46:44 -0500

    log usage

--- lib/ragios/monitors/plugins/metered_url_monitor.rb
+++ lib/ragios/monitors/plugins/metered_url_monitor.rb
@@ -7,17 +7,28 @@ class MeteredUrlMonitor
   attr_accessor :describe_test_result 
   attr_accessor :test_result
   attr_reader :url 
+  attr_reader :tag
   
   def init(options)
-      @user = options[:tag]
+      @tag = options[:tag]
       @url = options[:url] 
       raise "A url to test must be specified, url must be assigned a value" if @url.nil?    
       @describe_test_result = "HTTP GET Request to " + @url
   end
+
+  def log
+    data = {:tag => @tag, 
+            :date => Time.now.to_s(:long), 
+            :description => @describe_test_result,
+            :service =>'url_monitor'}
+ 
+     doc = {:database => 'usage_meter', :doc_id => UUIDTools::UUID.random_create.to_s, :data => data}
+     Couchdb.create_doc doc,Ragios::DatabaseAdmin.session
+  end
   
   def test_command
     begin
-     
+     log
      response = RestClient.get @url, {"User-Agent" => "Ragios (Saint-Ruby)"}
      @test_result = response.code
      return TRUE
