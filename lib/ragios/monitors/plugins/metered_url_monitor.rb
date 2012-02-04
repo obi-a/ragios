5e9df93534ce0961039920dc27bacc6ab1d71baa
Obi Akubue <obioraakubue@yahoo.com>
2012-02-04 01:53:16 -0500

    metered url generate invoice on the fly

--- lib/ragios/monitors/plugins/metered_url_monitor.rb
+++ lib/ragios/monitors/plugins/metered_url_monitor.rb
@@ -1,3 +1,5 @@
+require 'chronic'
+
 module Monitors
 
 #Plugin: Monitors a URL by sending a http GET request to it
@@ -10,20 +12,51 @@ class MeteredUrlMonitor
   attr_reader :tag
   
   def init(options)
-      @tag = options[:tag]
+      @user = options[:tag]
       @url = options[:url] 
       raise "A url to test must be specified, url must be assigned a value" if @url.nil?    
       @describe_test_result = "HTTP GET Request to " + @url
   end
 
+  def this_month
+    time = Time.now
+    time.strftime("%B")
+  end
+  
+  def this_year
+   time = Time.now
+   time.year.to_s
+  end
+
   def log
-    data = {:tag => @tag, 
-            :date => Time.now.to_s(:long), 
-            :description => @describe_test_result,
-            :service =>'url_monitor'}
+    charge_per_check = 0.0001
+    id = @user + this_month + this_year
+
+    begin
+    doc = {:database => 'invoice', :doc_id => id.to_s}
+    current_invoice = Couchdb.view doc,Ragios::DatabaseAdmin.session
+    num_of_checks = current_invoice['url_monitoring'] + 1
+    balance = current_invoice['balance'] + charge_per_check
+    balance.round_to(4)    
+
+    data = { :url_monitoring => num_of_checks, 
+             :balance => balance }
  
-     doc = {:database => 'usage_meter', :doc_id => UUIDTools::UUID.random_create.to_s, :data => data}
-     Couchdb.create_doc doc,Ragios::DatabaseAdmin.session
+    doc = { :database => 'invoice', :doc_id => id, :data => data}   
+    Couchdb.update_doc doc,Ragios::DatabaseAdmin.session
+    rescue CouchdbException => e
+       billing_period = this_month + ', ' + this_year 
+       data = {:username => @user, 
+               :billing_period => billing_period, 
+               :date_due => Chronic.parse('1st day next month'),
+               :balance => charge_per_check,
+               :url_monitoring => 1,
+               :status => 'not_due',
+             }
+ 
+     doc = {:database => 'invoice', :doc_id => id, :data => data}
+     Couchdb.create_doc doc,Ragios::DatabaseAdmin.session 
+    end
   end
   
   def test_command
