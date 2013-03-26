9f915b29b02deb8f00f3927aa6c141788028d308
obi-a <obioraakubue@yahoo.com>
2013-03-26 02:14:53 -0400

    added specs for amazon ses notifier

--- spec/notifier_config.rb
+++ spec/notifier_config.rb
@@ -5,6 +5,14 @@ gmail_account = {username: 'Gmail Username',
 Ragios::GmailNotifier.config(gmail_account)
 
 
+#Replace with amazon credientials for Amazon Simple Email Service Notifier
+amazon_account = { access_key: 'abc',
+                   secret_key: '123', 
+                   send_from: 'alerts <alerts@example.com>' }
+
+Ragios::SESNotifier.config(amazon_account)
+
+
 #replace with twitter credentials
 twitter_cred = {consumer_key: 'Consumer Key', 
                consumer_secret: 'Consumer secret',
