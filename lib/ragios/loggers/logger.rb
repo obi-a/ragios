c9f24435fe048ccc49f7cb9a064ed25de18e87f3
obi-a <obioraakubue@yahoo.com>
2013-05-28 02:47:59 -0400

    log all monitor activity when logging is enabled

--- lib/ragios/loggers/logger.rb
+++ lib/ragios/loggers/logger.rb
@@ -8,19 +8,18 @@ module Ragios
   end
 
   def write(monitor)
-    #update document with test information       
       data = {   
-         :test => monitor.test,
-         :timestamp => monitor.timestamp,
-         :test_result => monitor.describe_test_result, 
+         :monitor_id => monitor.id,
+         :test_description => monitor.test_description,
+         :timestamp => monitor.timestamp.to_s,
+         :describe_test_result => monitor.describe_test_result, 
          :time_of_test => monitor.time_of_last_test.to_s,
          :test_result => monitor.test_result.to_s, 
          :status => monitor.status,
          :tag => monitor.tag + Date.today.strftime("%B") + Date.today.strftime("%Y")
               }
-       id = "_" + monitor.id
-       doc = { :database => id, :doc_id => monitor.timestamp, :data => data}   
-       Couchdb.update_doc doc,Ragios::DatabaseAdmin.session    
+       doc = { :database => data[:monitor_id], :doc_id => data[:timestamp], :data => data}   
+       Couchdb.create_doc doc,Ragios::DatabaseAdmin.session    
   end 
 
   def log(monitor)
