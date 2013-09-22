7c84f5990db75ac97091ffdc37874ae2ec209e1b
obi-a <obioraakubue@yahoo.com>
2013-09-22 03:46:57 -0400

    deleted duplicate class

--- lib/ragios/server.rb
+++ lib/ragios/server.rb
@@ -1,12 +1,3 @@
-class Hash
-  #take keys of hash and transform those to a symbols
-  def self.transform_keys_to_symbols(value)
-    return value if not value.is_a?(Hash)
-    hash = value.inject({}){|memo,(k,v)| memo[k.to_sym] = Hash.transform_keys_to_symbols(v); memo}
-    return hash
-  end
-end
-
 #TODO: Ragios::Server may need to should have an exception class 
 #to allow clients like the RESTserver know exactly what a problem is with specific details.
 
