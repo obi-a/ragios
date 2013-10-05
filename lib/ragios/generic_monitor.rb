648c1c88c9b53ad3ceef8e047ed59bc6d4521dec
obi-a <obioraakubue@yahoo.com>
2013-10-05 14:26:32 -0400

    create plugin - Ragios::Monitors::System class no longer necessary

--- lib/ragios/generic_monitor.rb
+++ lib/ragios/generic_monitor.rb
@@ -1,65 +1,65 @@
 module Ragios 
-#Translates the Ragios Domain Specific Language to the object oriented system
-class GenericMonitor < Ragios::Monitors::System
+	class GenericMonitor 
 
-      attr_reader :plugin
-      attr_reader :options
-      attr_accessor :id
-      attr_accessor :tag
-      attr_accessor :status
-      attr_accessor :was_down
-      #attr_accessor :state
+  	attr_reader :plugin
+  	attr_reader :options
+  	attr_reader :id
+  	#attr_accessor :tag
+  	attr_accessor :status
+  	attr_accessor :was_down
+  	attr_accessor :state
 
-      #create the right type of monitor instance
-    def initialize(plugin,options)
-        @plugin = plugin
-        @plugin.ragios_init_values(options)
-        ragios_init_values(options)
-        @describe_test_result = ''
-        if defined?(@plugin.describe_test_result) 
-          @describe_test_result = @plugin.describe_test_result 
-        else
-          raise '@describe_test_result must be defined in ' + @plugin.to_s
-        end   
-        @options = options #to be used by the server scheduler 
-        create_notifier    
-        super()
+      
+    def initialize(options)
+      @options = options
+      create_plugin 
+      create_notifier    
     end
     
     def test_command
-        if @plugin.respond_to?('test_command')
-           status =  @plugin.test_command 
-           if defined?(@plugin.test_result)
-             @test_result = @plugin.test_result
-           else
-             raise '@test_result must be defined in ' + @plugin.to_s
-           end
+      if @plugin.respond_to?('test_command')
+        status =  @plugin.test_command 
+        if defined?(@plugin.test_result)
+          @test_result = @plugin.test_result
+        else
+          raise '@test_result must be defined in ' + @plugin.to_s
         end
-        return status
-    end
+     end
+     return status
+   end
 
-    def failed
-      unless @failed.nil?
+   def failed
+     unless @failed.nil?
        @failed.call if @failed.lambda?
-      end
-      @plugin.failed if @plugin.respond_to?('failed')
-    end
+     end
+     @plugin.failed if @plugin.respond_to?('failed')
+   end
      
-    def notify
-      @notifier.notify if @notifier.respond_to?('notify')
-    end
+   def notify
+     @notifier.notify if @notifier.respond_to?('notify')
+   end
 
-    def fixed
-      @notifier.resolved
-      unless @fixed.nil?
-        @fixed.call if @fixed.lambda?
-      end
-    end
+   def fixed
+     @notifier.resolved
+     unless @fixed.nil?
+       @fixed.call if @fixed.lambda?
+     end
+  end
 
 private
    def create_notifier
-     raise Ragios::NotifierNotFound.new(error: "No Notifier included"), "No Notifier included" unless @options.has_key?(:via)
-     @notifier = (Module.const_get("Ragios").const_get("Notifier").const_get(options[:via].camelize)).new(self)
+     raise Ragios::NotifierNotFound.new(error: "No Notifier Found"), "No Notifier Found" unless @options.has_key?(:via)
+     @notifier = (Module.const_get("Ragios").const_get("Notifier").const_get(@options[:via].camelize)).new(self)
+   end
+   
+   def create_plugin
+     raise Ragios::PluginNotFound.new(error: "No Plugin Found"), "No Notifier Found" unless @options.has_key?(:monitor)
+     module_name = "Monitors"  
+     plugin_name = @options[:monitor] 
+     plugin_class = Module.const_get("Ragios").const_get(module_name).const_get(plugin_name.camelize) 
+     plugin_class.new
+     plugin.init(@options)
+     @plugin  = plugin
    end
  end
 end
