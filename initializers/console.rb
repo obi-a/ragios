dcb316bda8b5272c37ff314def8350c982e58277
obi-a <obioraakubue@yahoo.com>
2013-10-26 04:10:43 -0400

    removed database creation on console loading

--- initializers/console.rb
+++ initializers/console.rb
@@ -1,6 +1,3 @@
-#create database if it doesn't already exisit
-Ragios::CouchdbAdmin.create_database
-
 #initialization code for the console
 Ragios::Controller.scheduler(Ragios::Scheduler.new(Ragios::Controller))
 Ragios::Controller.model(Ragios::Model::CouchdbMonitorModel)
