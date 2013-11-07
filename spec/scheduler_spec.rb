f83bfd3fd0f033b3e6219a42f48e4f87c7b89400
obi-a <obioraakubue@yahoo.com>
2013-11-06 21:24:42 -0500

    completed specs for new scheduler

--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -1,23 +1,28 @@
 require 'spec_base.rb'
 
 describe Ragios::Scheduler do
+  before(:each) do
+    @time_interval = '2m'
+    @tag = 'test'
+    @args = {:time_interval => @time_interval, :object => 'object', :tag => @tag}   
+  end
 
-  it "should schedule a controller to perform" do
-    time_interval = '2m'
-    tag = 'test'
-    args = {:time_interval => time_interval, :object => 'object', :tag => tag}
-    controller = double('controller')
-    controller.stub(:perform)
-    scheduler = Ragios::Scheduler.new(controller)
-    job = scheduler.schedule(args)
-    job.params[:tags].should == [tag]
-    job.t.should == time_interval
+  it "schedules a job" do
+    scheduler = Ragios::Scheduler.new('an object')
+    job = scheduler.schedule(@args)
+    job.params[:tags].should == [@tag]
+    job.t.should == @time_interval
     job.unschedule
   end
  
-  it "should unschedule a job" 
-  
-  it "should find a job by tag"
-  
-  it "should return all jobs"
+  it "unschedules a job" do
+    scheduler = Ragios::Scheduler.new('an object')
+    scheduler.schedule(@args)  
+    jobs = scheduler.find(@tag) 
+    jobs[0].params[:tags].should == [@tag]
+    jobs[0].t.should == @time_interval
+    scheduler.stop(@tag)
+    jobs = scheduler.find(@tag) 
+    jobs.should == []
+  end
 end
