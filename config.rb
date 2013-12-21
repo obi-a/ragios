31093d3fe13b684fc71fc5bdb719c63664180e17
obi-a <obioraakubue@yahoo.com>
2013-12-21 17:38:40 -0500

    remove auth_timeout from env variables to make setup easier

--- config.rb
+++ config.rb
@@ -3,28 +3,28 @@ require dir + 'lib/ragios'
 
 #Replace with Gmail username and password
 gmail_account = {username: ENV['GMAIL_USERNAME'],
-                 password: ENV['GMAIL_PASSWORD']} 
+                 password: ENV['GMAIL_PASSWORD']}
 
 Ragios::Notifier::GmailNotifier.config(gmail_account)
 
 #replace with twitter credentials
-twitter_cred = {consumer_key: ENV['TWITTER_CONSUMER_KEY'], 
+twitter_cred = {consumer_key: ENV['TWITTER_CONSUMER_KEY'],
                consumer_secret: ENV['TWITTER_CONSUMER_SECRET'],
                  access_token: ENV['TWITTER_ACCESS_TOKEN'],
                  access_secret: ENV['TWITTER_ACCESS_SECRET']}
 
-Ragios::Notifier::TwitterNotifier.config(twitter_cred)  
+Ragios::Notifier::TwitterNotifier.config(twitter_cred)
 
 #Replace with amazon credientials for Amazon Simple Email Service Notifier
 amazon_account = { access_key: ENV['AWS_ACCESS_KEY_ID'],
-                   secret_key: ENV['AWS_SECRET_ACCESS_KEY'], 
+                   secret_key: ENV['AWS_SECRET_ACCESS_KEY'],
                    send_from: "Ragios Alert <alerts@southmunn.com>" }
 
 Ragios::Notifier::Ses.config(amazon_account)
 
 ragios_admin_user = {username: ENV['RAGIOS_ADMIN_USERNAME'],
                      password: ENV['RAGIOS_ADMIN_PASSWORD'],
-                     auth_timeout: ENV['RAGIOS_ADMIN_AUTH_TIMEOUT'].to_i} 
+                     auth_timeout: 900}
 
 Ragios::Admin.config(ragios_admin_user)
 
@@ -36,6 +36,6 @@ database_admin = {login:     {username: ENV['COUCHDB_ADMIN_USERNAME'],
                                auth_session: 'ragios_auth_session'},
                   couchdb:  {bind_address: 'http://localhost',
                              port:'5984'}
-                 } 
+                 }
 
 Ragios::CouchdbAdmin.config(database_admin)
