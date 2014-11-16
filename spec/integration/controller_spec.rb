7b80c641f18f168cbcafaaa92d19152831647c47
The Rainmaker <obioraakubue@yahoo.com>
2014-11-15 23:41:36 -0500

    include assets and dependencies

--- spec/integration/controller_spec.rb
+++ spec/integration/controller_spec.rb
@@ -483,13 +483,13 @@ describe Ragios::Controller do
 
       sleep 1
 
-      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, event: "failed", notifier: "mock_notifier").count.should_not == 0
+      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, state: "failed", notifier: "mock_notifier").count.should_not == 0
 
       controller.update(monitor_id, plugin: "passing_plugin")
 
       sleep 1
 
-      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, event: "resolved").inspect
+      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, state: "resolved").inspect
 
       controller.delete(monitor_id)
     end
@@ -505,7 +505,7 @@ describe Ragios::Controller do
 
       sleep 1
 
-      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, event: "failed", notifier: "mock_notifier").count.should_not == 0
+      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, state: "failed", notifier: "mock_notifier").count.should_not == 0
 
       controller.stop(monitor_id)
       #stopped monitors are not restarted after an update
@@ -516,7 +516,7 @@ describe Ragios::Controller do
 
       sleep 1
 
-      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, event: "resolved").inspect
+      @database.where(type: "event", event_type: "monitor.notification", monitor_id: monitor_id, state: "resolved").inspect
 
       controller.delete(monitor_id)
     end
