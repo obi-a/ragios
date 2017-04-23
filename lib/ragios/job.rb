de032f78760970815967821bd2047c52fb4adf0f
Rainmaker <obioraakubue@yahoo.com>
2017-04-22 23:17:18 -0400

    add ragios recurring job

--- lib/ragios/job.rb
+++ lib/ragios/job.rb
@@ -1,14 +1,22 @@
 module Ragios
   class Job
+    include Celluloid
+
+    attr_reader :monitor_id, :interval, :timer
+
     def initialize(options)
+      @monitor_id = options[:monitor_id]
+      @interval = options[:interval]
     end
 
     def start
-      # start job to run on its schedule
+      @timer = every(interval) do
+        trigger_work
+      end
     end
 
     def trigger_work
-      # send message to worker to start work
+      puts "triggered work"
     end
   end
 end
