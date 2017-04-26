cd876ca80f46f2cc805b79e200c09f78d9277479
Rainmaker <obioraakubue@yahoo.com>
2017-04-25 23:04:07 -0400

    start jobs supervisor
    remove uuidtools dependency

--- lib/ragios/monitor_manager.rb
+++ lib/ragios/monitor_manager.rb
@@ -3,7 +3,7 @@ module Ragios
 
     attr_reader :monitor
 
-    def initialize(monitor)
+    def initialize(options)
       #add monitor to database
     end
 
@@ -14,12 +14,23 @@ module Ragios
 
     def schedule
       # spawns a new thread of the job to start running on its schedule
+      Ragios::Job.supervise_as(monitor_id, options)
     end
 
     def self.startup_monitors
       #runs during startup
       #load monitors from database
       #schedule them to run
+    end
+
+    private
+
+    def time
+      Time.now.utc
+    end
+
+    def unique_id
+      SecureRandom.uuid
     end
   end
 end
