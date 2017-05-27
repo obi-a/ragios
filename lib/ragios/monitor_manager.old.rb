fd3097245443d41584792c41603ee4c2b28593de
Rainmaker <obioraakubue@yahoo.com>
2017-05-27 11:06:19 -0400

    handle event publishing -add event publisher

---
+++ lib/ragios/monitor_manager.old.rb
@@ -0,0 +1,37 @@
+#re-write for controller.rbmodule Ragios
+module Ragios
+  class MonitorManager
+
+    attr_reader :monitor
+
+    def initialize(options)
+      #add monitor to database
+    end
+
+    def schedulable?
+      # checks if monitor can be scheduled
+      # runs the necessary validations
+    end
+
+    def schedule
+      # spawns a new thread of the job to start running on its schedule
+      Ragios::Job.supervise_as(monitor_id, options)
+    end
+
+    def self.startup_monitors
+      #runs during startup
+      #load monitors from database
+      #schedule them to run
+    end
+
+    private
+
+    def time
+      Time.now.utc
+    end
+
+    def unique_id
+      SecureRandom.uuid
+    end
+  end
+end
