403cde11ca164529259e92cd534028683e93c44e
Rainmaker <obioraakubue@yahoo.com>
2017-06-03 11:56:11 -0400

    add events_manager & events_job to manage to listen for events and write them to DB

---
+++ lib/ragios/events/events_job.rb
@@ -0,0 +1,24 @@
+module Ragios
+  class EventsJob
+    include Celluloid
+
+    def perform(options)
+      @event =  JSON.parse(options, symbolize_names: true)
+      log_event
+    end
+
+  private
+
+    def log_event
+      @model.save(unique_id, @event)
+    end
+
+    def unique_id
+      SecureRandom.uuid
+    end
+
+    def model
+      @model ||= Ragios::Database::Model.new(Ragios::CouchdbAdmin.get_database)
+    end
+  end
+end
