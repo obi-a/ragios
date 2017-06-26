17738924606c3e076a244adc4c1a49026cb8f30b
Rainmaker <obioraakubue@yahoo.com>
2017-06-26 02:26:40 -0400

    fix issues with daemonizing - fork before launching the actor
    - refactor notify job

--- bin/recurring_jobs_ctl.rb
+++ bin/recurring_jobs_ctl.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 require 'daemons'
-
 ragios_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-config = ragios_dir + '/config'
-require config
-receiver = Ragios::RecurringJobs::Receiver.new
-
-puts "starting out"
 Daemons.run_proc('recurring_jobs', log_output: true) do
 
+  config = ragios_dir + '/config'
+  require config
+
+  puts "starting out"
+
+  receiver = Ragios::RecurringJobs::Receiver.new
   #trap("INT") { puts "Shutting down."; pull.terminate; exit}
   trap 'TERM', lambda { puts "Shutting down."; receiver.terminate;}
   receiver.run
--- lib/ragios/notifications/notify_job.rb
+++ lib/ragios/notifications/notify_job.rb
@@ -3,29 +3,29 @@ module Ragios
     include Celluloid
 
     def perform(event, monitor, test_result, notifier)
-      begin
-        notifier.send(event, test_result) if notifier.respond_to?(event)
-        event_details = {
-          monitor_id: monitor[:_id],
-          state: event,
-          time: time.now.utc,
-          type: "event",
-          event_type: "monitor.notification",
-          monitor: monitor,
-          test_result: test_result,
-          notifier: notifier_name(notifier)
-        }
-      rescue Exception => exception
-        occurred = {"notifier error" => exception.message}
-        publisher.async.log_event(
-          event_details.merge(event: occurred)
-        )
-      end
+      event_details = {
+        monitor_id: monitor[:_id],
+        state: event,
+        time: time.now.utc,
+        type: "event",
+        event_type: "monitor.notification",
+        monitor: monitor,
+        test_result: test_result,
+        notifier: notifier_name(notifier)
+      }
+
+      notifier.send(event, test_result) if notifier.respond_to?(event)
+
       occurred = {notified: event, via: notifier}
       publisher.async.log_event(
         event_details.merge(event: occurred)
       )
-      terminate
+
+    rescue Exception => exception
+      occurred = {"notifier error" => exception.message}
+      publisher.async.log_event(
+        event_details.merge(event: occurred)
+      )
     end
 
   private
